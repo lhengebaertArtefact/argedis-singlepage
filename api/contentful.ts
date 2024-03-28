8f9661b2002d39a15730d12383a2c63b86c06c75
Léo HENGEBAERT <leo.hengebaert@FRARTDYF7L07WDW-M.local>
2024-03-28 17:28:30 +0100

    commenting contentful calls

--- api/contentful.ts
+++ api/contentful.ts
@@ -1,317 +1,317 @@
-const fetchContentfulData = async (query: string): Promise<any> => {
+// const fetchContentfulData = async (query: string): Promise<any> => {
 
-    const res: any = await fetch(
-      `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+//     const res: any = await fetch(
+//       `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
   
-      {
-        method: "POST",
-        headers: {
-          'Authorization': `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ query }),
-      }
-    );
-    const data = res.json();
-    return data;
-  };
+//       {
+//         method: "POST",
+//         headers: {
+//           'Authorization': `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ query }),
+//       }
+//     );
+//     const data = res.json();
+//     return data;
+//   };
   
-  export const getAllPages = async (): Promise<any | null> => {
+//   export const getAllPages = async (): Promise<any | null> => {
   
-    let query = ` {
-      pageCollection {
-        items {
-          title
-          sys {
-            id
-          }
-          logo {
-            url
-          }
-          cardContent {
-            json
-          }
-          cardImage {
-            url
-          }
-          producersRefCollection(limit: 10) {
-            limit
-            items {
-              ... on Producer {
-                sys {
-                  id
-                }
-                producer
-                buttonText
-                producerPhoto {
-                  url
-                }
-                producerDescription {
-                  json
-                }
-              }
-            }
-          }
-        }
-      }
-    }`;
-    const res = await fetchContentfulData(query);
-    const pages: any = res.data.pageCollection.items || [];
+//     let query = ` {
+//       pageCollection {
+//         items {
+//           title
+//           sys {
+//             id
+//           }
+//           logo {
+//             url
+//           }
+//           cardContent {
+//             json
+//           }
+//           cardImage {
+//             url
+//           }
+//           producersRefCollection(limit: 10) {
+//             limit
+//             items {
+//               ... on Producer {
+//                 sys {
+//                   id
+//                 }
+//                 producer
+//                 buttonText
+//                 producerPhoto {
+//                   url
+//                 }
+//                 producerDescription {
+//                   json
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }`;
+//     const res = await fetchContentfulData(query);
+//     const pages: any = res.data.pageCollection.items || [];
   
-    // Si vous attendez une seule page, vous pouvez simplement retourner la première
-    // page de la liste ou null si la liste est vide
-    return pages;
-  }
+//     // Si vous attendez une seule page, vous pouvez simplement retourner la première
+//     // page de la liste ou null si la liste est vide
+//     return pages;
+//   }
   
-  export const getPage = async (locale: string): Promise<any | null> => {
-    const transformLocale = locale === "fr" ? "fr" : "en-US"
-    let query = ` {
-      pageCollection(locale : "${transformLocale}") {
-        items {
-          title
-          sys {
-            id
-          }
-          logo {
-            url
-          }
-          cardContent {
-            json
-          }
-          cardImage {
-            url
-          }
-          producersRefCollection(limit: 10) {
-            limit
-            items {
-              ... on Producer {
-                sys {
-                  id
-                }
-                producer
-                buttonText
-                producerPhoto {
-                  url
-                }
-                producerDescription {
-                  json
-                }
-              }
-            }
-          }
-        }
-      }
-    }`;
-    const res = await fetchContentfulData(query);
-    const pages: any = res.data.pageCollection.items || [];
+//   export const getPage = async (locale: string): Promise<any | null> => {
+//     const transformLocale = locale === "fr" ? "fr" : "en-US"
+//     let query = ` {
+//       pageCollection(locale : "${transformLocale}") {
+//         items {
+//           title
+//           sys {
+//             id
+//           }
+//           logo {
+//             url
+//           }
+//           cardContent {
+//             json
+//           }
+//           cardImage {
+//             url
+//           }
+//           producersRefCollection(limit: 10) {
+//             limit
+//             items {
+//               ... on Producer {
+//                 sys {
+//                   id
+//                 }
+//                 producer
+//                 buttonText
+//                 producerPhoto {
+//                   url
+//                 }
+//                 producerDescription {
+//                   json
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }`;
+//     const res = await fetchContentfulData(query);
+//     const pages: any = res.data.pageCollection.items || [];
   
-    // Si vous attendez une seule page, vous pouvez simplement retourner la première
-    // page de la liste ou null si la liste est vide
-    return pages;
-  }
+//     // Si vous attendez une seule page, vous pouvez simplement retourner la première
+//     // page de la liste ou null si la liste est vide
+//     return pages;
+//   }
   
-  export const getAllRegionsId = async (slug: string): Promise<any | null> => {
-    let query = `{
-      fr: pageCollection(where: {sys: { id: "${slug}" } }, locale: "fr") {
-        items {
-          sys {
-            id
-          }
-          title
-          logo {
-            url
-          }
-          cardContent {
-            json
-          }
-          cardImage {
-            url
-          }
-          producersRefCollection(limit: 10) {
-            limit
-            items {
-              ... on Producer {
-                sys {
-                  id
-                }
-                producer
-                buttonText
-                producerPhoto {
-                  url
-                }
-                producerDescription {
-                  json
-                }
-              }
-            }
-          }
-        }
-      }
-      en: pageCollection(where: {sys: { id: "${slug}" } }, locale: "en-US") {
-        items {
-          sys {
-            id
-          }
-          title
-          logo {
-            url
-          }
-          cardContent {
-            json
-          }
-          cardImage {
-            url
-          }
-          producersRefCollection(limit: 10) {
-            limit
-            items {
-              ... on Producer {
-                sys {
-                  id
-                }
-                producer
-                buttonText
-                producerPhoto {
-                  url
-                }
-                producerDescription {
-                  json
-                }
-              }
-            }
-          }
-        }
-      }
-    }`;
+//   export const getAllRegionsId = async (slug: string): Promise<any | null> => {
+//     let query = `{
+//       fr: pageCollection(where: {sys: { id: "${slug}" } }, locale: "fr") {
+//         items {
+//           sys {
+//             id
+//           }
+//           title
+//           logo {
+//             url
+//           }
+//           cardContent {
+//             json
+//           }
+//           cardImage {
+//             url
+//           }
+//           producersRefCollection(limit: 10) {
+//             limit
+//             items {
+//               ... on Producer {
+//                 sys {
+//                   id
+//                 }
+//                 producer
+//                 buttonText
+//                 producerPhoto {
+//                   url
+//                 }
+//                 producerDescription {
+//                   json
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//       en: pageCollection(where: {sys: { id: "${slug}" } }, locale: "en-US") {
+//         items {
+//           sys {
+//             id
+//           }
+//           title
+//           logo {
+//             url
+//           }
+//           cardContent {
+//             json
+//           }
+//           cardImage {
+//             url
+//           }
+//           producersRefCollection(limit: 10) {
+//             limit
+//             items {
+//               ... on Producer {
+//                 sys {
+//                   id
+//                 }
+//                 producer
+//                 buttonText
+//                 producerPhoto {
+//                   url
+//                 }
+//                 producerDescription {
+//                   json
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }`;
   
-    const res = await fetchContentfulData(query);
-    return { fr: res.data.fr.items, en: res.data.en.items } || null;
-  }
+//     const res = await fetchContentfulData(query);
+//     return { fr: res.data.fr.items, en: res.data.en.items } || null;
+//   }
   
   
   
-  export const getRegion = async (slug: any, locale: string): Promise<any | null> => {
+//   export const getRegion = async (slug: any, locale: string): Promise<any | null> => {
   
-    const transformLocale = locale === "fr" ? "fr" : "en-US"
+//     const transformLocale = locale === "fr" ? "fr" : "en-US"
   
-      let query = `{
-        pageCollection(where: {sys: { id: "${slug}" } }, locale : "${transformLocale}") {
-        items {
-            sys {
-              id
-            }
-            title
-            logo {
-              url
-            }
-            cardContent {
-              json
-            }
-            cardImage {
-              url
-            }
+//       let query = `{
+//         pageCollection(where: {sys: { id: "${slug}" } }, locale : "${transformLocale}") {
+//         items {
+//             sys {
+//               id
+//             }
+//             title
+//             logo {
+//               url
+//             }
+//             cardContent {
+//               json
+//             }
+//             cardImage {
+//               url
+//             }
     
-            producersRefCollection(limit: 10) {
-              limit
-              items {
-                ... on Producer {
-                  sys {
-                    id
-                  }
-                  producer
-                  buttonText
-                  producerPhoto {
-                    url
-                  }
-                  producerDescription {
-                    json
-                  }
-                }
-              }
-            }
-          }
-        }
-      }`;
-      const res = await fetchContentfulData(query);
-      return res.data.pageCollection.items[0] || null;
-  }
+//             producersRefCollection(limit: 10) {
+//               limit
+//               items {
+//                 ... on Producer {
+//                   sys {
+//                     id
+//                   }
+//                   producer
+//                   buttonText
+//                   producerPhoto {
+//                     url
+//                   }
+//                   producerDescription {
+//                     json
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }`;
+//       const res = await fetchContentfulData(query);
+//       return res.data.pageCollection.items[0] || null;
+//   }
   
-  export const getAllProducerId = async (slug: any): Promise<any | null> => {
+//   export const getAllProducerId = async (slug: any): Promise<any | null> => {
   
-    let query = `{
-      producerCollection(where : { sys : { id: "${slug}"}}) {
-        items {
-          sys {
-            id
-          }
-          producer
-          producerPhoto {
-            url
-          }
-          producerDescription{
-            json
-          }
-        }
-      }
-    }`;
+//     let query = `{
+//       producerCollection(where : { sys : { id: "${slug}"}}) {
+//         items {
+//           sys {
+//             id
+//           }
+//           producer
+//           producerPhoto {
+//             url
+//           }
+//           producerDescription{
+//             json
+//           }
+//         }
+//       }
+//     }`;
   
-    const res = await fetchContentfulData(query);
-    const producer: any | null = res.data.producerCollection?.items.map((element : any) => element.sys.id)
+//     const res = await fetchContentfulData(query);
+//     const producer: any | null = res.data.producerCollection?.items.map((element : any) => element.sys.id)
   
-    return producer;
-  };
+//     return producer;
+//   };
   
-  export const getAllProducersNames = async (): Promise<any | null> => {
+//   export const getAllProducersNames = async (): Promise<any | null> => {
   
-    let query = `{
-      producerCollection {
-        items {
-          sys {
-            id
-          }
-          producer
-          producerPhoto {
-            url
-          }
-          producerDescription{
-            json
-          }
-        }
-      }
-    }`;
+//     let query = `{
+//       producerCollection {
+//         items {
+//           sys {
+//             id
+//           }
+//           producer
+//           producerPhoto {
+//             url
+//           }
+//           producerDescription{
+//             json
+//           }
+//         }
+//       }
+//     }`;
   
-    const res = await fetchContentfulData(query);
-    const producer: any | null = res.data.producerCollection?.items.map((element: any) => element.producer)
+//     const res = await fetchContentfulData(query);
+//     const producer: any | null = res.data.producerCollection?.items.map((element: any) => element.producer)
   
-    return producer;
-  };
+//     return producer;
+//   };
   
-  export const getProducerName = async (slug: any, locale: string): Promise<any | null> => {
+//   export const getProducerName = async (slug: any, locale: string): Promise<any | null> => {
   
-    const transformLocale = locale === "fr" ? "fr" : "en-US"
+//     const transformLocale = locale === "fr" ? "fr" : "en-US"
   
-    let query = `{
-      producerCollection(where : { sys : { id: "${slug}"}}, locale: "${transformLocale}") {
-      items {
-        sys {
-            id
-          }
-          producer
-          producerPhoto {
-            url
-          }
-          producerDescription{
-            json
-          }
-        }
-      }
-    }`;
+//     let query = `{
+//       producerCollection(where : { sys : { id: "${slug}"}}, locale: "${transformLocale}") {
+//       items {
+//         sys {
+//             id
+//           }
+//           producer
+//           producerPhoto {
+//             url
+//           }
+//           producerDescription{
+//             json
+//           }
+//         }
+//       }
+//     }`;
   
-    const res = await fetchContentfulData(query);
-    const producer: any | null = res.data.producerCollection?.items[0] || null
+//     const res = await fetchContentfulData(query);
+//     const producer: any | null = res.data.producerCollection?.items[0] || null
   
-    return producer;
-  };
+//     return producer;
+//   };
   
   
   
